da56c0f97fd6bdf94e2290d1ebb86e776e8029ad
Stepz <nine.chaitavat@gmail.com>
2025-04-18 05:03:14 +0700

    ✨ add active section context and hooks for improved section tracking

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { motion } from "framer-motion"
-import { links } from "@/lib/data"
-import Link from "next/link"
+import React from "react";
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -14,25 +19,40 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-              <motion.li
-                className='h-3/4 flex items-center justify-center'
-                key={link.hash}
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y:0, opacity: 1 }}
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition",
+                  { "text-slate-950": activeSection === link.name }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
               >
-                <Link
-                  className='flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition'
-                  href={link.hash}
-                >
-                  {link.name}
-                </Link>
-              </motion.li>
-            ))}
+                {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-slate-100 rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{ type: "spring", stiffness: 380, damping: 30 }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
         </ul>
       </nav>
     </header>
-  )
+  );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -7,11 +7,14 @@ import SIcon from "@/public/MyPic.jpg";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin, BsGithub } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.7);
+
   return (
     <section
+      ref={ref}
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
